a78757085f966fc0f72553c1a4e691f723a84fcd
M0doku <danlll@bk.ru>
2024-10-27 12:02:40 +0500

    Load to git

---
+++ OrdersFilteringApp/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace OrdersFilteringApp
+{
+	partial class OrdersFilteringApp
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+			GetOrdersFromTextFileButton = new Button();
+			OrdersListDataGrid = new DataGridView();
+			DistrictComboBox = new ComboBox();
+			FilterTimeButton = new Button();
+			SaveOrdersToTextFileButton = new Button();
+			GetOrdersFromDbFileButton = new Button();
+			GetOrdersLabel = new Label();
+			CancelFilteringButton = new Button();
+			SelectedDistrictLabel = new Label();
+			SaveOrdersToDbFileButton = new Button();
+			SaveOrdersLabel = new Label();
+			((System.ComponentModel.ISupportInitialize)OrdersListDataGrid).BeginInit();
+			SuspendLayout();
+			// 
+			// GetOrdersFromTextFileButton
+			// 
+			GetOrdersFromTextFileButton.Location = new Point(614, 46);
+			GetOrdersFromTextFileButton.Name = "GetOrdersFromTextFileButton";
+			GetOrdersFromTextFileButton.Size = new Size(120, 25);
+			GetOrdersFromTextFileButton.TabIndex = 0;
+			GetOrdersFromTextFileButton.Text = "Текстовый файл";
+			GetOrdersFromTextFileButton.UseVisualStyleBackColor = true;
+			GetOrdersFromTextFileButton.Click += GetOrdersFromTextFileButton_Click;
+			// 
+			// OrdersListDataGrid
+			// 
+			dataGridViewCellStyle1.NullValue = null;
+			OrdersListDataGrid.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+			OrdersListDataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+			OrdersListDataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			OrdersListDataGrid.Location = new Point(12, 12);
+			OrdersListDataGrid.Name = "OrdersListDataGrid";
+			OrdersListDataGrid.Size = new Size(555, 300);
+			OrdersListDataGrid.TabIndex = 1;
+			// 
+			// DistrictComboBox
+			// 
+			DistrictComboBox.FormattingEnabled = true;
+			DistrictComboBox.Location = new Point(614, 135);
+			DistrictComboBox.Name = "DistrictComboBox";
+			DistrictComboBox.RightToLeft = RightToLeft.No;
+			DistrictComboBox.Size = new Size(120, 23);
+			DistrictComboBox.TabIndex = 2;
+			// 
+			// FilterTimeButton
+			// 
+			FilterTimeButton.Location = new Point(614, 195);
+			FilterTimeButton.Name = "FilterTimeButton";
+			FilterTimeButton.Size = new Size(120, 25);
+			FilterTimeButton.TabIndex = 4;
+			FilterTimeButton.Text = "Отфильтровать";
+			FilterTimeButton.UseVisualStyleBackColor = true;
+			FilterTimeButton.Click += FilterTimeButton_Click;
+			// 
+			// SaveOrdersToTextFileButton
+			// 
+			SaveOrdersToTextFileButton.Location = new Point(614, 256);
+			SaveOrdersToTextFileButton.Name = "SaveOrdersToTextFileButton";
+			SaveOrdersToTextFileButton.Size = new Size(120, 25);
+			SaveOrdersToTextFileButton.TabIndex = 5;
+			SaveOrdersToTextFileButton.Text = "Текстовый файл";
+			SaveOrdersToTextFileButton.UseVisualStyleBackColor = true;
+			SaveOrdersToTextFileButton.Click += SaveOrdersToTextFileButton_Click;
+			// 
+			// GetOrdersFromDbFileButton
+			// 
+			GetOrdersFromDbFileButton.Location = new Point(614, 77);
+			GetOrdersFromDbFileButton.Name = "GetOrdersFromDbFileButton";
+			GetOrdersFromDbFileButton.Size = new Size(120, 25);
+			GetOrdersFromDbFileButton.TabIndex = 6;
+			GetOrdersFromDbFileButton.Text = "Файл базы данных";
+			GetOrdersFromDbFileButton.UseVisualStyleBackColor = true;
+			GetOrdersFromDbFileButton.Click += GetOrdersFromDbFileButton_Click;
+			// 
+			// GetOrdersLabel
+			// 
+			GetOrdersLabel.Location = new Point(614, 12);
+			GetOrdersLabel.Name = "GetOrdersLabel";
+			GetOrdersLabel.Size = new Size(120, 30);
+			GetOrdersLabel.TabIndex = 7;
+			GetOrdersLabel.Text = "Получить заказы через:";
+			GetOrdersLabel.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// CancelFilteringButton
+			// 
+			CancelFilteringButton.Location = new Point(599, 164);
+			CancelFilteringButton.Name = "CancelFilteringButton";
+			CancelFilteringButton.Size = new Size(150, 25);
+			CancelFilteringButton.TabIndex = 8;
+			CancelFilteringButton.Text = "Отменить фильтрацию";
+			CancelFilteringButton.UseVisualStyleBackColor = true;
+			CancelFilteringButton.Click += CancelFilteringButton_Click;
+			// 
+			// SelectedDistrictLabel
+			// 
+			SelectedDistrictLabel.Location = new Point(614, 110);
+			SelectedDistrictLabel.Name = "SelectedDistrictLabel";
+			SelectedDistrictLabel.Size = new Size(120, 15);
+			SelectedDistrictLabel.TabIndex = 9;
+			SelectedDistrictLabel.Text = "Выбранный район:";
+			SelectedDistrictLabel.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// SaveOrdersToDbFileButton
+			// 
+			SaveOrdersToDbFileButton.Location = new Point(614, 287);
+			SaveOrdersToDbFileButton.Name = "SaveOrdersToDbFileButton";
+			SaveOrdersToDbFileButton.Size = new Size(120, 25);
+			SaveOrdersToDbFileButton.TabIndex = 10;
+			SaveOrdersToDbFileButton.Text = "Файл базы данных";
+			SaveOrdersToDbFileButton.UseVisualStyleBackColor = true;
+			SaveOrdersToDbFileButton.Click += SaveOrdersToDbFileButton_Click;
+			// 
+			// SaveOrdersLabel
+			// 
+			SaveOrdersLabel.Location = new Point(614, 223);
+			SaveOrdersLabel.Name = "SaveOrdersLabel";
+			SaveOrdersLabel.Size = new Size(120, 30);
+			SaveOrdersLabel.TabIndex = 11;
+			SaveOrdersLabel.Text = "Сохранить заказы как:";
+			SaveOrdersLabel.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// OrdersFilteringApp
+			// 
+			AutoScaleMode = AutoScaleMode.None;
+			ClientSize = new Size(770, 317);
+			Controls.Add(SaveOrdersLabel);
+			Controls.Add(SaveOrdersToDbFileButton);
+			Controls.Add(SelectedDistrictLabel);
+			Controls.Add(CancelFilteringButton);
+			Controls.Add(GetOrdersLabel);
+			Controls.Add(GetOrdersFromDbFileButton);
+			Controls.Add(SaveOrdersToTextFileButton);
+			Controls.Add(FilterTimeButton);
+			Controls.Add(DistrictComboBox);
+			Controls.Add(OrdersListDataGrid);
+			Controls.Add(GetOrdersFromTextFileButton);
+			FormBorderStyle = FormBorderStyle.Fixed3D;
+			MaximizeBox = false;
+			Name = "OrdersFilteringApp";
+			SizeGripStyle = SizeGripStyle.Hide;
+			Text = "OrdersFilteringApp";
+			((System.ComponentModel.ISupportInitialize)OrdersListDataGrid).EndInit();
+			ResumeLayout(false);
+		}
+
+		#endregion
+
+		private Button GetOrdersFromTextFileButton;
+		private ComboBox DistrictComboBox;
+		public DataGridView OrdersListDataGrid;
+		private Button FilterTimeButton;
+		private Button SaveOrdersToTextFileButton;
+		private Button GetOrdersFromDbFileButton;
+		private Label GetOrdersLabel;
+		private Button CancelFilteringButton;
+		private Label SelectedDistrictLabel;
+		private Button SaveOrdersToDbFileButton;
+		private Label SaveOrdersLabel;
+	}
+}
---
+++ OrdersFilteringApp/Form1.cs
@@ -0,0 +1,259 @@
+using System.Diagnostics;
+using System.IO;
+using Serilog;
+using Microsoft.EntityFrameworkCore;
+using static OrdersFilteringApp.OrdersFilteringApp;
+
+namespace OrdersFilteringApp
+{
+	public partial class OrdersFilteringApp : Form
+	{
+		List<Order> ordersList = new List<Order>();
+		List<Order> filteredOrdersList = new List<Order>();
+		static string DbPath = AppDomain.CurrentDomain.BaseDirectory + "_deliveryOrders.db";
+		public OrdersFilteringApp()
+		{
+			InitializeComponent();
+			string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmm");
+			if(!Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "/Logs/"))
+			{
+				Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "/Logs/");
+			}
+			Log.Logger = new LoggerConfiguration()
+				.WriteTo.File($"{AppDomain.CurrentDomain.BaseDirectory}/Logs/_deliveryLog_{timestamp}.txt")
+				.CreateLogger();
+			Log.Information("Program started");
+		}
+
+
+		private void GetOrdersFromTextFile()
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+			ofd.Filter = "txt files (*.txt)|*.txt";
+			if (ofd.ShowDialog() == DialogResult.OK)
+			{
+				using (StreamReader sr = new StreamReader(ofd.FileName))
+				{
+					Order order = new Order();
+					string line;
+					if (sr.Peek == null)
+					{
+						MessageBox.Show("В файле нет данных");
+						Log.Information("No data in the file.");
+					}
+					else
+					{
+						ordersList.Clear();
+					}
+					while ((line = sr.ReadLine()!) != null)
+					{
+						try
+						{
+							var values = line.Split(' ');
+							int id = Int32.Parse(values[0]);
+							double weight = double.Parse(values[1]);
+							DateTime dt = DateTime.ParseExact(values[3] + ' ' + values[4], "yyyy-MM-dd HH:mm:ss",
+										   System.Globalization.CultureInfo.InvariantCulture);
+							ordersList.Add(order = new Order(id, weight, values[2], dt));
+							GetDistricts();
+							Log.Information($"Order added: ID: {order.ID}, Weight: {order.Weight}, District: {order.District}, Time: {order.Time}");
+						}
+						catch (Exception ex)
+						{
+							MessageBox.Show($"Некорректные данные были пропущены.");
+							Log.Error("Error is happened: " + ex.Message);
+						}
+					}
+					OrdersListDataGrid.DataSource = null;
+				}
+				foreach (var order in ordersList)
+				{
+					Debug.WriteLine($"ID: {order.ID}, Weight: {order.Weight}, District: {order.District}, Time: {order.Time}");
+				}
+			}
+		}
+
+		private void GetOrdersFromDbFile()
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+			ofd.Filter = "db files (*.db)|*.db";
+			if (ofd.ShowDialog() == DialogResult.OK)
+			{
+				File.Copy(ofd.FileName, DbPath, true);
+				using (ApplicationContext db = new ApplicationContext())
+				{
+					try
+					{
+						ordersList = db.Orders.ToList();
+						OrdersListDataGrid.DataSource = ordersList;
+						OrdersListDataGrid.Columns["Time"].DefaultCellStyle.Format = "G";
+						GetDistricts();
+						foreach (var order in db.Orders)
+						{
+							Log.Information($"Order added: ID: {order.ID}, Weight: {order.Weight}, District: {order.District}, Time: {order.Time}");
+						}
+					}
+					catch (Exception ex)
+					{
+						MessageBox.Show("Выберите файл в корректном формате.");
+						Log.Error("Error is happened: " + ex.Message);
+					}
+				}
+			}
+
+		}
+
+		private void FilteringOrders()
+		{
+			string selectedDistrict = string.Empty;
+			if (DistrictComboBox.SelectedValue?.ToString() != null)
+			{
+				selectedDistrict = DistrictComboBox.SelectedValue.ToString();
+				var firstOrder = ordersList.Where(x => x.District == selectedDistrict).OrderBy(x => x.Time).First();
+				Debug.WriteLine($"ID: {firstOrder.ID}, Weight: {firstOrder.Weight}, District: {firstOrder.District}, Time: {firstOrder.Time}");
+				filteredOrdersList = ordersList.Where(x => x.District == selectedDistrict).Where(x => x.Time >= firstOrder.Time && x.Time <= firstOrder.Time.AddMinutes(30)).OrderBy(x => x.Time).ToList();
+				OrdersListDataGrid.DataSource = filteredOrdersList;
+				Log.Information($"Orders filtered by district: {selectedDistrict}");
+			}
+			else
+			{
+				MessageBox.Show("Выберите район для фильтрации.");
+				Log.Information("No data to filter.");
+			}
+		}
+
+		private void SaveOrdersToTextFile()
+		{
+			SaveFileDialog sfd = new SaveFileDialog();
+			sfd.Filter = "txt files (*.txt)|*.txt";
+			sfd.FileName = "_deliveryOrder";
+			if (sfd.ShowDialog() == DialogResult.OK)
+			{
+				using (StreamWriter sw = new StreamWriter(sfd.FileName))
+				{
+					if (filteredOrdersList.Count() > 0)
+					{
+						foreach (var order in filteredOrdersList)
+						{
+							sw.WriteLine($"{order.ID} {order.Weight} {order.District} {order.Time}");
+							Log.Information($"Order saved: ID: {order.ID} Weight: {order.Weight} District: {order.District} Time: {order.Time}");
+						}
+					}
+					else
+					{
+						foreach (var order in ordersList)
+						{
+							sw.WriteLine($"{order.ID} {order.Weight} {order.District} {order.Time}");
+							Log.Information($"Order saved: ID: {order.ID} Weight: {order.Weight} District: {order.District} Time: {order.Time}");
+						}
+					}
+					Log.Information($"Orders saved to file {sfd.FileName}");
+				}
+			}
+		}
+
+		private void SaveOrdersToDbFile()
+		{
+			SaveFileDialog sfd = new SaveFileDialog();
+			sfd.Filter = "db files (*.db)|*.db";
+			sfd.FileName = "_deliveryOrder";
+			if (sfd.ShowDialog() == DialogResult.OK)
+			{
+				using (ApplicationContext db = new ApplicationContext())
+				{
+					db.Database.ExecuteSqlRaw("DELETE FROM Orders");
+					if (filteredOrdersList.Count > 0)
+					{
+						foreach (var order in filteredOrdersList)
+						{
+							db.Orders.Add(order);
+							Log.Information($"Order saved: ID: {order.ID} Weight: {order.Weight} District: {order.District} Time: {order.Time}");
+						}
+					}
+					else
+					{
+						foreach (var order in ordersList)
+						{
+							db.Orders.Add(order);
+							Log.Information($"Order saved: ID: {order.ID} Weight: {order.Weight} District: {order.District} Time: {order.Time}");
+						}
+
+					}
+					db.SaveChanges();
+				}
+				File.Copy(DbPath, sfd.FileName, true);
+			}
+		}
+
+		private void GetOrdersFromTextFileButton_Click(object sender, EventArgs e)
+		{
+			GetOrdersFromTextFile();
+			OrdersListDataGrid.DataSource = ordersList;
+			OrdersListDataGrid.Columns["Time"].DefaultCellStyle.Format = "G";
+		}
+		public class Order
+		{
+			public int ID { get; set; }
+			public double Weight { get; set; }
+			public string District { get; set; }
+			public DateTime Time { get; set; }
+			public Order(int ID, double Weight, string District, DateTime Time)
+			{
+				this.ID = ID;
+				this.Weight = Weight;
+				this.District = District;
+				this.Time = Time;
+			}
+			public Order() { }
+		}
+
+		public partial class ApplicationContext : DbContext
+		{
+			public DbSet<Order> Orders { get; set; } = null!;
+			public ApplicationContext() => Database.EnsureCreated();
+			protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+			{
+				optionsBuilder.UseSqlite($"Data Source={DbPath}");
+			}
+
+		}
+
+		private void GetDistricts()
+		{
+			var districts = ordersList.Select(x => x.District);
+			var uniqueDistricts = districts.Distinct().ToList();
+			DistrictComboBox.DataSource = uniqueDistricts;
+			foreach (var district in uniqueDistricts)
+			{
+				Log.Information($"District loaded: {district}");
+			}
+		}
+
+		private void FilterTimeButton_Click(object sender, EventArgs e)
+		{
+			FilteringOrders();
+		}
+
+		private void SaveOrdersToTextFileButton_Click(object sender, EventArgs e)
+		{
+			SaveOrdersToTextFile();
+		}
+
+		private void GetOrdersFromDbFileButton_Click(object sender, EventArgs e)
+		{
+			GetOrdersFromDbFile();
+		}
+
+		private void CancelFilteringButton_Click(object sender, EventArgs e)
+		{
+			OrdersListDataGrid.DataSource = ordersList;
+			Log.Information("Orders restored.");
+		}
+
+		private void SaveOrdersToDbFileButton_Click(object sender, EventArgs e)
+		{
+			SaveOrdersToDbFile();
+		}
+
+	}
+}
